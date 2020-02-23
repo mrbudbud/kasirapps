d636d06b997beb6e4c47d3accc0c67646ab8c611
bram26 <bram.17411071@student.ubl.ac.id>
2020-02-23 14:25:54 +0700

    hsjh

--- routes/web.php
+++ routes/web.php
@@ -20,13 +20,9 @@ Route::group(['middleware' => ['auth','ceklevel:1']], function(){
 
     Route::get('/home', 'HomeController@index')->name('home');
     Route::get('/customer', 'TableCustomerController@index')->name('homeCustomer');
-<<<<<<< HEAD
-    Route::post('/insertcustomer', 'TableCustomerController@store')->name('insertCustomer');
-=======
     Route::post('/customer/insertcustomer', 'TableCustomerController@store')->name('insertCustomer');
     Route::get('/customer/cari', 'TableCustomerController@cari')->name('cariCustomer');
     Route::get('/customer/formedit/{id}', 'TableCustomerController@edit')->name('formEditCustomer');
     Route::post('/customer/update/{id}', 'TableCustomerController@update')->name('updateCustomer');
->>>>>>> c3ab45c3ade052b1a179669f7932e72892572e17
 
 });
